07b953487171d3776ef648b48f63af6d4f65ab47
ShiroNek0 <lamnsse@gmail.com>
2023-04-08 23:43:40 +0900

    Update better solution for ValiPalindrome

--- src/00125_ValidPalindrome.py
+++ src/00125_ValidPalindrome.py
@@ -1,12 +1,7 @@
-# https://leetcode.com/problems/valid-palindrome/submissions/927285775/
-
-import math
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
         s= "".join(ch for ch in s if ch.isalnum())
-        print(s)
         strLen = len(s)
 
         if strLen <= 1:
@@ -23,4 +18,33 @@ class Solution:
             j -= 1
 
         return True
-    
+
+
+# import math
+
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s = s.lower()
+#         s= "".join(ch for ch in s if ch.isalnum())
+#         print(s)
+#         strLen = len(s)
+
+#         if strLen <= 1:
+#             return True
+
+#         middle = math.ceil(strLen/2) - 1
+#         if strLen%2 == 0:
+#             p1 = middle 
+#             p2 = middle + 1
+#         else:
+#             p1 = middle - 1
+#             p2 = middle + 1
+
+#         for i in range(p2, strLen):
+#             if s[p1] != s[p2]:
+#                 return False
+#             else:
+#                 p1 -= 1
+#                 p2 += 1
+
+#         return True
